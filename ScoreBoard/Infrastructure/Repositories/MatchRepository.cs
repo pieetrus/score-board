4cac40466b7806e5762b3a65634855e7754b548d
Jakub Pietrus <pietrusjakub@gmail.com>
2024-05-09 19:05:48 +0200

    [GREEN] Implement MatchRepository.

--- ScoreBoard/Infrastructure/Repositories/MatchRepository.cs
+++ ScoreBoard/Infrastructure/Repositories/MatchRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ScoreBoard.Domain.Interfaces;
 using ScoreBoard.Domain.Models;
+using ScoreBoard.Infrastructure.Exceptions;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ScoreBoard.Tests")]
@@ -7,29 +8,42 @@ namespace ScoreBoard.Infrastructure.Repositories
 {
     internal sealed class MatchRepository : IMatchRepository
     {
+        private readonly List<Match> _matches = new();
+
         public void Add(Match match)
         {
-            throw new NotImplementedException();
+            if (GetByTeamNames(match.HomeTeam, match.AwayTeam) is not null)
+            {
+                throw new MatchBeetweenTheseTeamsAlreadyExistsException(match.HomeTeam, match.AwayTeam);
+            }
+
+            _matches.Add(match);
         }
 
-        public IEnumerable<Match> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Match> GetAll() => _matches;
 
         public Match? GetByTeamNames(string teamHomeName, string teamAwayName)
-        {
-            throw new NotImplementedException();
-        }
+            => _matches.FirstOrDefault(m => m.HomeTeam == teamHomeName && m.AwayTeam == teamAwayName);
 
         public void Remove(Match match)
         {
-            throw new NotImplementedException();
+            var existingMatch = GetByTeamNames(match.HomeTeam, match.AwayTeam);
+
+            if (existingMatch is null)
+            {
+                throw new MatchDoesNotExistsException(match.HomeTeam, match.AwayTeam);
+            }
+
+            _matches.Remove(existingMatch);
         }
 
         public void Update(Match match)
         {
-            throw new NotImplementedException();
+            var existingMatch = GetByTeamNames(match.HomeTeam, match.AwayTeam);
+            if (existingMatch is not null)
+            {
+                existingMatch.UpdateScores(match.HomeScore, match.AwayScore);
+            }
         }
     }
 }
